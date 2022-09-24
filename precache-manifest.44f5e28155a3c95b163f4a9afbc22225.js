f3af1b765e6c01e252e64d49f60616d7a4a60ba2
prajyot1398 <prajyot13.98@gmail.com>
2022-09-24 18:18:47 +0530

    Updates

---
+++ precache-manifest.44f5e28155a3c95b163f4a9afbc22225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1eb552cc67f7eb74ad2c",
+    "url": "/textutils/static/css/main.70abb658.chunk.css"
+  },
+  {
+    "revision": "1eb552cc67f7eb74ad2c",
+    "url": "/textutils/static/js/main.1eb552cc.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/textutils/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "f7f7ed366a2bc62cedb3",
+    "url": "/textutils/static/js/2.f7f7ed36.chunk.js"
+  },
+  {
+    "revision": "7115dd224cc4f8d54ce5",
+    "url": "/textutils/static/js/runtime~main.7115dd22.js"
+  },
+  {
+    "revision": "f1b0b9c0772be09795fb74ea6e8a5ff0",
+    "url": "/textutils/index.html"
+  }
+];
